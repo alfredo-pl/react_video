344bd570cedee91dc66f9acb9fc6c9e15d96f473
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-06 13:48:07 -0400

    add query term to method onTermSeach

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import SearchBar from './SearchBar/SearchBar'
+import SearchBar from './SearchBar/SearchBar';
+import youtube from './api/youtube';
 
 
 export default class App extends React.Component {
+
+    onTermSearch= term =>{
+        youtube.get('/search',{
+            params:{
+                q: term
+            }
+        })
+    }
+
     render(){
         return(
             <div className="ui container">
-                <SearchBar/>
+                <SearchBar onFormSubmit={this.onTermSearch}/>
             </div>
         )
     }
