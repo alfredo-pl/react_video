3d24adeab920ac4616c66c6acc1e8ffe8b3b3140
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-07 21:43:17 -0400

    the utility brings the element where the image and the title are extracted

--- src/components/VideoItem/VideoItem.js
+++ src/components/VideoItem/VideoItem.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const VideoItem = () => {
-    return ( <div>Video Item</div> );
+const VideoItem = ({video}) => {
+    return (
+        <div>
+            <h6>{video.snippet.title}</h6>
+            <img src={video.snippet.thumbnails.medium.url}/>
+        </div>
+    )
 }
  
 export default VideoItem;
