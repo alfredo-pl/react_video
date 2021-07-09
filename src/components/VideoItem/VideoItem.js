082ebee5c72281257b8ee09e808326ce237bccc9
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-09 09:48:47 -0400

    style to component video item

--- src/components/VideoItem/VideoItem.js
+++ src/components/VideoItem/VideoItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({video}) => {
     return (
-        <div>
-            <h6>{video.snippet.title}</h6>
-            <img src={video.snippet.thumbnails.medium.url}/>
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
     )
 }
