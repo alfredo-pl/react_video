6b42dae44905a2ebebd7c1c50149d54228266764
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-10 09:16:28 -0400

    implemented grid sistem

--- src/components/App.js
+++ src/components/App.js
@@ -27,11 +27,18 @@ export default class App extends React.Component {
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSearch}/>
             
-                I have {this.state.videos.length} videos.
-                <VideoDetail video={this.state.seletedVideo}/>
-                <VideoList 
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}/>
+               <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={this.state.seletedVideo}/>
+                    </div>
+                    <div className="five wide column">
+                        <VideoList 
+                        onVideoSelect={this.onVideoSelect}
+                        videos={this.state.videos}/>
+                    </div>
+                </div>
+               </div>
             </div>
         )
     }
